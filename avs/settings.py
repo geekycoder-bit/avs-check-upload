11b37549e13bc121e3bfa4f3b5064ed4cf4db448
geekycoder-bit <ahmad00rocks@gmail.com>
2021-03-17 16:07:54 +0530

    latest Commit

--- avs/settings.py
+++ avs/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '5%-frhc^b5g91ap&!elaz0f$fp#c3n_%@r5(ilp=24c6^z$nm='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://avs-upload-check.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,11 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT=os.path.join(BASE_DIR, 'home/assets')
+
 STATIC_URL = '/static/'
 
 STATIC_FILES_DIRS=os.path.join(BASE_DIR, 'home/static')
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'home/assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
